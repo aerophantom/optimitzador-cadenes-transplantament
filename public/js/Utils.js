4df4681a97cce745a5f1f365d1a9f17f7b127b10
Luis Ka <aerophantom@outlool.com>
2018-08-11 18:46:57 +0200

    Afegida documentació a Utils.js

--- public/js/Utils.js
+++ public/js/Utils.js
@@ -1,4 +1,17 @@
+/**
+ * Class that contains useful functions.
+ */
 class Utils{
+
+    /**
+     * Obtains the current datetime MySQL formatted.
+     *
+     * More information:
+     * https://stackoverflow.com/questions/10211145/getting-current-date-and-time-in-javascript
+     *
+     * @returns {string}
+     * @public
+     */
     static get currentDateTime() {
         let now     = new Date();
         let year    = now.getFullYear();
@@ -24,16 +37,39 @@ class Utils{
         }
         return year+'/'+month+'/'+day+' '+hour+':'+minute+':'+second;
     }
-
 }
 
+/**
+ * Obtains a formatted string. All the '{}' occurrences are replaced by
+ * the parameters passed by.
+ *
+ * For example: "This {} was retrieved from {}.".format("code", "StackOverflow")
+ * will return: "This code was retrieved from StackOverflow".
+ *
+ * More information:
+ * https://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
+ *
+ * @returns {string} - formatted string.
+ */
 String.prototype.format = function() {
-    // https://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
-    // estaria de puta madre indicar como furula esta, es un reduce.
-    // mas info: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
     return [...arguments].reduce((p,c) => p.replace(/{}/,c), this);
 };
 
-// String.prototype.hashCode() = function()
-//TODO estaria be fer un Utils.js Podria afegir la funcio que genera el
-// hashcode
+/**
+ * Obtains the string's hash code. Based on the same function on Java.
+ *
+ * More information:
+ * https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
+ *
+ * @returns {number}
+ */
+String.prototype.hashCode = function() {
+    var hash = 0, i, chr;
+    if (this.length === 0) return hash;
+    for (i = 0; i < this.length; i++) {
+        chr   = this.charCodeAt(i);
+        hash  = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+};
