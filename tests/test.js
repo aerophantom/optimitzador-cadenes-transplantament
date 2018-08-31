143d0c9a3fd0ebad9774a343be56fd56dccafd43
Luis Ka <aerophantom@outlool.com>
2018-08-31 22:33:16 +0200

    Tests: per obtenir el fitxer actualitzat

--- tests/test.js
+++ tests/test.js
@@ -236,7 +236,7 @@ test('get donant successors\'', t => {
 
 });
 
-test('testObtenirSuccessorsDeReceptor', t => {
+test('get successor of recipient', t => {
     let to = new TransplantOptimizer(file);
     to.inicialitzarReceptors();
     to.inicialitzarDonants();
@@ -272,6 +272,305 @@ test('get score', t => {
 
     let score = to.scoreMultipleDonors({donant: "3000", receptor: "2002"});
     t.is(score, 59.59);
+});
+
+test('get updated file', t => {
+    let expected = JSON.parse(JSON.stringify(file));
+    let to = new TransplantOptimizer(file);
+    t.deepEqual(to.update, expected);
+});
+
+test('get updated file ignoring a donor', t => {
+    let expected = {
+        "origin": "TESTS",
+        "description": "Fitxer arcs5.json. Arc de prova amb només 5 receptors i les dades mínimes per provar la aplicació. Els id dels donants altruistes es correspoan amb el codi 1xxx, els receptors amb el codi 2xxx i els donants associats amb el codi 3xxx",
+        "altruists": [
+            "1000",
+            "1001"
+        ],
+        "patients": {
+            "2000": {
+                "related_donors": [
+                    "3000",
+                    "3001"
+                ],
+                "compatible_donors": [
+                    {
+                        "failure_prob": 0.2,
+                        "score": 58.95,
+                        "donor": "1000"
+                    },
+                    {
+                        "failure_prob": 0.2,
+                        "score": 38.95,
+                        "donor": "3001"
+                    },
+                    {
+                        "failure_prob": 0.15,
+                        "score": 60.50,
+                        "donor": "3003"
+                    }
+                ]
+            },
+            "2001": {
+                "related_donors": [
+                ],
+                "compatible_donors": [
+                    {
+                        "failure_prob": 0.2,
+                        "score": 89.55,
+                        "donor": "3000"
+                    },
+                    {
+                        "failure_prob": 0.1,
+                        "score": 54.88,
+                        "donor": "3003"
+                    }
+                ]
+            },
+            "2002": {
+                "related_donors": [
+                    "3003",
+                    "3004",
+                    "3005"
+                ],
+                "compatible_donors": [
+                    {
+                        "failure_prob": 0.2,
+                        "score": 33.54,
+                        "donor": "1001"
+                    },
+                    {
+                        "failure_prob": 0.2,
+                        "score": 59.59,
+                        "donor": "3000"
+                    },
+                    {
+                        "failure_prob": 0.5,
+                        "score": 24.32,
+                        "donor": "3004"
+                    },
+                    {
+                        "failure_prob": 0.1,
+                        "score": 76.30,
+                        "donor": "3006"
+                    }
+                ]
+            },
+            "2003": {
+                "related_donors": [
+                    "3006"
+                ],
+                "compatible_donors": [
+                    {
+                        "failure_prob": 0.5,
+                        "score": 24.29,
+                        "donor": "3001"
+                    },
+                    {
+                        "failure_prob": 0.2,
+                        "score": 44.55,
+                        "donor": "3007"
+                    },
+                    {
+                        "failure_prob": 0.5,
+                        "score": 24.29,
+                        "donor": "3008"
+                    }
+                ]
+            },
+            "2004": {
+                "related_donors": [
+                    "3007",
+                    "3008"
+                ],
+                "compatible_donors": [
+                    {
+                        "failure_prob": 0.1,
+                        "score": 50.88,
+                        "donor": "3000"
+                    },
+                    {
+                        "failure_prob": 0.5,
+                        "score": 68.23,
+                        "donor": "3006"
+                    }
+                ]
+            }
+        }
+    };
+    let to = new TransplantOptimizer(file);
+    to._IgnoredDonors = ["3002"];
+    t.deepEqual(to.update, expected);
+});
+
+test('get updated file ignoring a recipient', t => {
+    let expected = {
+        "origin": "TESTS",
+        "description": "Fitxer arcs5.json. Arc de prova amb només 5 receptors i les dades mínimes per provar la aplicació. Els id dels donants altruistes es correspoan amb el codi 1xxx, els receptors amb el codi 2xxx i els donants associats amb el codi 3xxx",
+        "altruists": [
+            "1000",
+            "1001"
+        ],
+        "patients": {
+            "2001": {
+                "related_donors": [
+                    "3002"
+                ],
+                "compatible_donors": [
+                    {
+                        "failure_prob": 0.1,
+                        "score": 54.88,
+                        "donor": "3003"
+                    }
+                ]
+            },
+            "2002": {
+                "related_donors": [
+                    "3003",
+                    "3004",
+                    "3005"
+                ],
+                "compatible_donors": [
+                    {
+                        "failure_prob": 0.2,
+                        "score": 33.54,
+                        "donor": "1001"
+                    },
+                    {
+                        "failure_prob": 0.5,
+                        "score": 24.32,
+                        "donor": "3004"
+                    },
+                    {
+                        "failure_prob": 0.1,
+                        "score": 76.30,
+                        "donor": "3006"
+                    }
+                ]
+            },
+            "2003": {
+                "related_donors": [
+                    "3006"
+                ],
+                "compatible_donors": [
+                    {
+                        "failure_prob": 0.2,
+                        "score": 44.55,
+                        "donor": "3007"
+                    },
+                    {
+                        "failure_prob": 0.5,
+                        "score": 24.29,
+                        "donor": "3008"
+                    }
+                ]
+            },
+            "2004": {
+                "related_donors": [
+                    "3007",
+                    "3008"
+                ],
+                "compatible_donors": [
+                    {
+                        "failure_prob": 0.5,
+                        "score": 74.11,
+                        "donor": "3002"
+                    },
+                    {
+                        "failure_prob": 0.5,
+                        "score": 68.23,
+                        "donor": "3006"
+                    }
+                ]
+            }
+        }
+    };
+    let to = new TransplantOptimizer(file);
+    to._IgnoredRecipients = ["2000"];
+    t.deepEqual(to.update, expected);
+});
+
+test('get updated file ignoring a recipient and a donor', t => {
+    let expected = {
+        "origin": "TESTS",
+        "description": "Fitxer arcs5.json. Arc de prova amb només 5 receptors i les dades mínimes per provar la aplicació. Els id dels donants altruistes es correspoan amb el codi 1xxx, els receptors amb el codi 2xxx i els donants associats amb el codi 3xxx",
+        "altruists": [
+            "1000",
+            "1001"
+        ],
+        "patients": {
+            "2001": {
+                "related_donors": [
+                    "3002"
+                ],
+                "compatible_donors": [
+                    {
+                        "failure_prob": 0.1,
+                        "score": 54.88,
+                        "donor": "3003"
+                    }
+                ]
+            },
+            "2002": {
+                "related_donors": [
+                    "3003",
+                    "3004",
+                    "3005"
+                ],
+                "compatible_donors": [
+                    {
+                        "failure_prob": 0.2,
+                        "score": 33.54,
+                        "donor": "1001"
+                    },
+                    {
+                        "failure_prob": 0.5,
+                        "score": 24.32,
+                        "donor": "3004"
+                    },
+                    {
+                        "failure_prob": 0.1,
+                        "score": 76.30,
+                        "donor": "3006"
+                    }
+                ]
+            },
+            "2003": {
+                "related_donors": [
+                    "3006"
+                ],
+                "compatible_donors": [
+                    {
+                        "failure_prob": 0.5,
+                        "score": 24.29,
+                        "donor": "3008"
+                    }
+                ]
+            },
+            "2004": {
+                "related_donors": [
+                    "3008"
+                ],
+                "compatible_donors": [
+                    {
+                        "failure_prob": 0.5,
+                        "score": 74.11,
+                        "donor": "3002"
+                    },
+                    {
+                        "failure_prob": 0.5,
+                        "score": 68.23,
+                        "donor": "3006"
+                    }
+                ]
+            }
+        }
+    };
+    let to = new TransplantOptimizer(file);
+    to._IgnoredRecipients = ["2000"];
+    to._IgnoredDonors = ["3007"];
+    t.deepEqual(to.update, expected);
 });
 
 test('positive crossed test', t => {
